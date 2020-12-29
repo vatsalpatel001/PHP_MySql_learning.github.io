3544e0a88c7312d482e9045461ae27ee527a2654
vatsal <vatsalpatel001@gmail.com>
2020-12-29 17:16:46 +0530

    strutre updated

---
+++ script.js
@@ -0,0 +1,25 @@
+	
+function  trigger(){
+	// alert("j")
+	const Head = document.querySelectorAll(".Card_Short");
+	const SubStr = document.querySelectorAll(".Card_Long");
+   Head.forEach((elem,index,arr) => {
+    arr[index].setAttribute("vat-id","MSD_"+index)
+    elem.addEventListener("click", start);
+    });
+      SubStr.forEach((elem,index,arr) => {
+      	 arr[index].setAttribute("id","Virat_"+index);
+      	 arr[index].setAttribute("class","Card_Long hide");
+    elem.addEventListener("dblclick", hider);
+    });
+   // alert(Head.length)
+}
+
+function start(){
+
+	document.getElementById(event.target.getAttribute("vat-id").replace("MSD","Virat")).classList.toggle("hide")
+}
+
+function hider(){
+	event.target.classList.toggle("hide")
+}
